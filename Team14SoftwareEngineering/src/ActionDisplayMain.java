f56df459135ab960f91caacfe978b3a716e61f3a
Jibaspoudel <jibaspoudel143@gmail.com>
2022-03-10 14:54:27 -0600

    Added Action Display class

---
+++ Team14SoftwareEngineering/src/ActionDisplayMain.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JFrame;
+
+
+/**
+ *
+ * @author JIBAS POUDEL
+ */
+public class ActionDisplayMain {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        ActionDisplay test = new ActionDisplay();
+        
+        frame.setSize(700, 500);
+        frame.setVisible(true);
+        frame.add(test);
+    }
+}
