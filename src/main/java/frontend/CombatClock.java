e2e417d0f6a1b7373755ed5fee13b40c43da80f8
cicarter <cicarter@uark.edu>
2022-03-10 16:07:16 -0600

    I got the combatClock file from @endertuber. Will have everyone recommit tomorrow so that the correct author will be credited.

---
+++ src/main/java/frontend/CombatClock.java
@@ -0,0 +1,37 @@
+package frontend;
+
+import javax.swing.*;
+import java.awt.*;
+import java.text.DecimalFormat;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class CombatClock extends JLabel{
+    public boolean timeUp = false;  //If you want something to happen when countdown reaches 0, check for this being true.
+
+    CombatClock(){
+        super();
+    }
+
+    public void StartCombat(){  //This should be called when the action starts. Lasts for 6 minutes by default.
+        Timer timer = new Timer();
+
+        timer.scheduleAtFixedRate(new TimerTask(){
+            int i = 360;
+            DecimalFormat secondFormat = new DecimalFormat("00");
+            public void run() {
+                setText((i/60) + ":" + secondFormat.format(i%60));
+                i--;
+                if(i < 31){
+                    setForeground(Color.RED);
+                }
+                if(i < 0){
+                    timer.cancel();
+                    timeUp = true;
+                    setText("Game Over");
+                }
+            }
+        }, 0, 1000);
+
+    }
+}
