cd63eb95664d5779c36e316ec34ada4017ef3610
Jibaspoudel <jibaspoudel143@gmail.com>
2022-03-10 15:06:30 -0600

    Added action display

--- Team14SoftwareEngineering/src/ActionDisplay.java
+++ Team14SoftwareEngineering/src/ActionDisplay.java
@@ -7,7 +7,6 @@ public class ActionDisplay extends javax.swing.JPanel {
     public ActionDisplay() {
         initComponents();
     }
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
